11fd6ab511d374f3beaa528c976174227d8a691d
LeeChanHui <chanhui@n3cloud.co.kr>
2024-08-28 22:53:38 +0900

    test

--- src/main/java/project/study_with_me/domain/controller/StudyController.java
+++ src/main/java/project/study_with_me/domain/controller/StudyController.java
@@ -1,7 +1,6 @@
 package project.study_with_me.domain.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +8,6 @@ import project.study_with_me.domain.service.StudyService;
 
 @Controller
 @RequiredArgsConstructor
-@Slf4j
 public class StudyController {
 
     @Value("${kakao.client_id}")
@@ -19,7 +17,12 @@ public class StudyController {
 
     @PostMapping("/kakaoLogin")
     public String test(@RequestBody String code) {
-        log.info(code);
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@" + code);
         return code;
+    }
+
+    @GetMapping("/test")
+    public String test1() {
+        return "hi";
     }
 }
