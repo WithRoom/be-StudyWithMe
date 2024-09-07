52dd7c3dbc5edc17b1f24ef010925a98d0157e56
LeeChanHui <chanhui@n3cloud.co.kr>
2024-09-07 23:28:20 +0900

    [오류수정] : Getter 추가g

--- src/main/java/project/study_with_me/s3/dto/ImageUrlResponseDto.java
+++ src/main/java/project/study_with_me/s3/dto/ImageUrlResponseDto.java
@@ -1,10 +1,12 @@
 package project.study_with_me.s3.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class ImageUrlResponseDto {
 
     private String ImageUrl;
