e0ef6fd92761e561eb2db567ac641c050c983d6a
LeeChanHui <chanhui@n3cloud.co.kr>
2024-09-08 19:44:55 +0900

    [오류수정] : CreateStudyRequestDto 수정

--- src/main/java/project/study_with_me/domain/study/dto/request/StudySchedule.java
+++ src/main/java/project/study_with_me/domain/study/dto/request/StudySchedule.java
@@ -1,8 +1,12 @@
 package project.study_with_me.domain.study.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudySchedule {
 
     private String weekDay; // 요일
