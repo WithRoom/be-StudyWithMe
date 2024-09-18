a172149c78b0a3004367538ef42c5588ca6c5127
LeeChanHui <chanhui@n3cloud.co.kr>
2024-09-18 17:23:04 +0900

    fix: CommentRequestDto 에 memberId 삭제

--- src/main/java/project/study_with_me/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/project/study_with_me/domain/comment/dto/CommentRequestDto.java
@@ -3,6 +3,7 @@ package project.study_with_me.domain.comment.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import project.study_with_me.auth.jwt.utils.SecurityUtil;
 import project.study_with_me.domain.comment.entity.Comment;
 
 @Getter
@@ -11,14 +12,13 @@ import project.study_with_me.domain.comment.entity.Comment;
 public class CommentRequestDto {
 
     private Long studyId;
-    private Long memberId;
     private String content;
 
     public Comment createComment() {
         return Comment.builder()
                 .content(content)
                 .studyId(studyId)
-                .memberId(memberId)
+                .memberId(SecurityUtil.getCurrentMemberId())
                 .build();
     }
 }
